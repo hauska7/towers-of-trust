b9d4aa4d67512a29a4ed5ce2b1ad9a114f516a64
Grzegorz Hauska <grzegorz.hauska@gmail.com>
2018-11-20 10:37:32 +0100

    Dont use devise confirmable for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable #, :confirmable
   belongs_to :city, optional: true
   has_many :cities, class_name: "City"
   has_many :participations
